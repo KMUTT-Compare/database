89f1c1b18dfe439833e8d0804360a0890ef40d3a
iceage38 <nanthawan.ice31@mail.kmutt.ac.th>
2025-02-09 20:53:18 +0700

    editJS

--- dbinsert.js
+++ dbinsert.js
@@ -1,9 +1,9 @@
-//STAFFS
+// staffs
 db.staff.insertMany([
   {
     "staffId": 1,
     "staffName": "Somchai Jaid",
-    "address": {
+    "staffAddress": {
 	  "dormNumber": "111",
       "street": "123/4 Sukhumvit Road",
       "subdistrict": "Khlong Tan",
@@ -11,13 +11,13 @@ db.staff.insertMany([
       "province": "Bangkok",
       "postalCode": "10110"
     },
-    "email": "somchai.jaid@gmail.com",
-    "phone": "0812345678"
+    "staffEmail": "somchai.jaid@gmail.com",
+    "staffPhone": "0812345678"
   },
   {
     "staffId": 2,
     "staffName": "Ornthai Suayngam",
-    "address": {
+    "staffAddress": {
 	  "dormNumber": "222",
       "street": "456/7 Rama IX Road",
       "subdistrict": "Suan Luang",
@@ -25,34 +25,34 @@ db.staff.insertMany([
       "province": "Bangkok",
       "postalCode": "10250"
     },
-    "email": "ornthai.suayngam@gmail.com",
-    "phone": "0823456789"
+    "staffEmail": "ornthai.suayngam@gmail.com",
+    "staffPhone": "0823456789"
   }
 ]);
 
-// USERS
+// users
 db.user.insertMany([
-    {
-      "userId": 1,
-      "username": "john_doe",
-      "password": "password123",
-      "email": "john.doe@example.com",
-      "role": "admin",
-      "createdOn": new Date("2024-05-11T12:00:00Z"), 
-      "updatedOn": new Date("2024-05-31T12:00:00Z")  
-    },
-    {
-      "userId": 2,
-      "username": "jane_smith",
-      "password": "securePass456",
-      "email": "jane.smith@example.com",
-      "role": "user",
-      "createdOn": new Date("2024-06-15T12:00:00Z"), 
-      "updatedOn": new Date("2024-07-08T12:00:00Z")  
-    }
-  ]);
+  {
+    "userId": 1,
+    "username": "john_doe",
+    "password": "password123",
+    "email": "john.doe@example.com",
+    "role": "admin",
+    "createdOn": new Date("2024-05-11T12:00:00Z"), 
+    "updatedOn": new Date("2024-05-31T12:00:00Z")  
+  },
+  {
+    "userId": 2,
+    "username": "jane_smith",
+    "password": "securePass456",
+    "email": "jane.smith@example.com",
+    "role": "user",
+    "createdOn": new Date("2024-06-15T12:00:00Z"), 
+    "updatedOn": new Date("2024-07-08T12:00:00Z")  
+  }
+]);
 
-//  FAVORITES
+//  favorites
   db.favorite.insertMany([
     {
       "favId": 1,
@@ -65,8 +65,38 @@ db.user.insertMany([
       "userId": 2      
     }
   ]);
+
+// forms
+db.form.insertMany([
+  {
+    formId: 1,
+    form_date: new Date("2025-01-15"),
+    name: "somchai123",
+    email: "somchai@example.com",
+    phone: "0812345678",
+    date_in: new Date("2025-01-20"),
+    date_out: new Date("2025-01-25"),
+	description: "good dormitory",
+    staffId: 1,
+    dormId: 1,
+	userId: 1
+  },
+  {
+    formId: 2,
+    form_date: new Date("2025-01-16"),
+    name: "arisa999",
+    email: "arisa@example.com",
+    phone: "0865432109",
+    date_in: new Date("2025-01-22"),
+    date_out: new Date("2025-01-28"),
+	description: "what about your new dormitory?",
+    staffId: 2,
+    dormId: 2,
+	userId: 2
+  }
+]);
   
-//DORMITORIES
+// dormitorys
 db.dormitory.insertOne({
     "dormId": 1,
     "name": "The Cozy Dorm",
@@ -88,46 +118,10 @@ db.dormitory.insertOne({
     "created_at": new Date("2024-11-15T12:00:00Z"),
     "updated_at": new Date("2024-11-04T12:00:00Z"),
     "image": [
-      "https://example.com/image1.jpg"
+      
     ],
     "building_facility": ["CCTV","WiFi","24-hour security"],
     "room_facility": ["Air conditioning","Private bathroom","Desk"],
     "staffId": 1
 });
 
-
-db.dormitory.insertOne({
-    "dormId": 2,
-    "name": "Student Residence",
-    "status": "full",
-    "address": {
-	  "dormNumber": "222",
-      "street": "Phahonyothin Road",
-      "subdistrict": "Chatuchak",
-      "district": "Chatuchak",
-      "province": "Bangkok",
-      "postalCode": "10900"
-    },
-    "roomCount": 30,
-    "type": "f",
-    "size": Decimal128.fromString("20.0"), // ใช้ Decimal128
-    "min_price": Decimal128.fromString("4000.00"), // ใช้ Decimal128
-    "max_price": Decimal128.fromString("6000.00"), // ใช้ Decimal128
-    "distance": Decimal128.fromString("2.0"), // ใช้ Decimal128
-    "created_at": new Date("2024-08-10T12:00:00Z"),
-    "updated_at": new Date("2024-09-01T12:00:00Z"),
-    "image": [
-      "https://example.com/image3.jpg"
-    ],
-    "building_facility": [
-      "Gym",
-      "Laundry",
-      "Study room"
-    ],
-    "room_facility": [
-      "Wardrobe",
-      "Refrigerator",
-      "WiFi"
-    ],
-    "staffId": 2
-  });
--- dbscript.js
+++ dbscript.js
@@ -1,9 +1,10 @@
+// staff
 db.createCollection("staff", {
   validator: {
     $jsonSchema: {
       "bsonType": "object",
       "title": "staff",
-      "required": ["staffId", "staffName", "address", "email", "phone"],
+      "required": ["staffId", "staffName", "staffAddress", "staffEmail", "staffPhone"],
       "properties": {
         "staffId": {
           "bsonType": "int"
@@ -11,7 +12,7 @@ db.createCollection("staff", {
         "staffName": {
           "bsonType": "string"
         },
-        "address": {
+        "staffAddress": {
           "bsonType": "object",
           "title": "object",
           "required": ["dormNumber", "street", "subdistrict", "district", "province", "postalCode"],
@@ -36,160 +37,211 @@ db.createCollection("staff", {
             }
           }
         },
-        "email": {
+        "staffEmail": {
           "bsonType": "string"
         },
-        "phone": {
+        "staffPhone": {
           "bsonType": "string"
         }
       }
     }
   }
 });
-//   
-  db.createCollection("user", {
-    validator: {
-      $jsonSchema: {
-        "bsonType": "object",
-        "title": "user",
-        "required": ["userId", "username", "password", "email", "role", "createdOn", "updatedOn"],
-        "properties": {
-          "userId": {
-            "bsonType": "int"
-          },
-          "username": {
-            "bsonType": "string"
-          },
-          "password": {
-            "bsonType": "string"
-          },
-          "email": {
-            "bsonType": "string"
-          },
-          "role": {
-            "enum": ['admin', 'user', 'guest']
-          },
-          "createdOn": {
-            "bsonType": "date"
-          },
-          "updatedOn": {
-            "bsonType": "date"
-          }
+// user
+db.createCollection("user", {
+  validator: {
+    $jsonSchema: {
+      "bsonType": "object",
+      "title": "user",
+      "required": ["userId", "username", "password", "email", "role", "createdOn", "updatedOn"],
+      "properties": {
+        "userId": {
+          "bsonType": "int"
+        },
+        "username": {
+          "bsonType": "string"
+        },
+        "password": {
+          "bsonType": "string"
+        },
+        "email": {
+          "bsonType": "string"
+        },
+        "role": {
+          "enum": ['admin', 'user', 'guest']
+        },
+        "createdOn": {
+          "bsonType": "date"
+        },
+        "updatedOn": {
+          "bsonType": "date"
         }
       }
     }
-  });
-//   
-  db.createCollection("dormitory", {
-    validator: {
-      $jsonSchema: {
-        "bsonType": "object",
-        "title": "dormitory",
-        "required": ["dormId", "name", "status", "address", "roomCount", "type", "size", "min_price", "max_price", "distance", "created_at", "updated_at", "building_facility", "room_facility", "staffId"],
-        "properties": {
-          "dormId": {
-            "bsonType": "int"
-          },
-          "name": {
+  }
+});
+// dormitory
+db.createCollection("dormitory", {
+  validator: {
+    $jsonSchema: {
+      "bsonType": "object",
+      "title": "dormitory",
+      "required": ["dormId", "dormName", "status", "address", "roomCount", "type", "size", "min_price", "max_price", "distance", "created_at", "updated_at", "building_facility", "room_facility", "count_facilities",  "score", "staffId"],
+      "properties": {
+        "dormId": {
+          "bsonType": "int"
+        },
+        "dormName": {
+          "bsonType": "string"
+        },
+        "status": {
+          "enum": ['empty', 'full']
+        },
+        "address": {
+          "bsonType": "object",
+          "title": "object",
+          "required": ["dormNumber","street", "subdistrict", "district", "province", "postalCode"],
+          "properties": {
+			"dormNumber": {
+              "bsonType": "string"
+            },
+            "street": {
+              "bsonType": "string"
+            },
+            "subdistrict": {
+              "bsonType": "string"
+            },
+            "district": {
+              "bsonType": "string"
+            },
+            "province": {
+              "bsonType": "string"
+            },
+            "postalCode": {
+              "bsonType": "string"
+            }
+          }
+        },
+        "roomCount": {
+          "bsonType": "int"
+        },
+        "type": {
+          "enum": ['all', 'f', 'm']
+        },
+        "size": {
+          "bsonType": "decimal"
+        },
+        "min_price": {
+          "bsonType": "decimal"
+        },
+        "max_price": {
+          "bsonType": "decimal"
+        },
+        "distance": {
+          "bsonType": "decimal"
+        },
+        "created_at": {
+          "bsonType": "date"
+        },
+        "updated_at": {
+          "bsonType": "date"
+        },
+        "image": {
+          "bsonType": "array",
+          "items": {
             "bsonType": "string"
-          },
-          "status": {
-            "enum": ['empty', 'full']
-          },
-          "address": {
-            "bsonType": "object",
-            "title": "object",
-            "required": ["dormNumber","street", "subdistrict", "district", "province", "postalCode"],
-            "properties": {
-              "dormNumber": {
-                "bsonType": "string"
-              },
-              "street": {
-                "bsonType": "string"
-              },
-              "subdistrict": {
-                "bsonType": "string"
-              },
-              "district": {
-                "bsonType": "string"
-              },
-              "province": {
-                "bsonType": "string"
-              },
-              "postalCode": {
-                "bsonType": "string"
-              }
-            }
-          },
-          "roomCount": {
-            "bsonType": "int"
-          },
-          "type": {
-            "enum": ['all', 'f', 'm']
-          },
-          "size": {
-            "bsonType": "decimal"
-          },
-          "min_price": {
-            "bsonType": "decimal"
-          },
-          "max_price": {
-            "bsonType": "decimal"
-          },
-          "distance": {
-            "bsonType": "decimal"
-          },
-          "created_at": {
-            "bsonType": "date"
-          },
-          "updated_at": {
-            "bsonType": "date"
-          },
-          "image": {
-            "bsonType": "array",
-            "items": {
-              "bsonType": "string"
-            }
-          },
-          "building_facility": {
-            "bsonType": "array",
-            "items": {
-              "bsonType": "string"
-            }
-          },
-          "room_facility": {
-            "bsonType": "array",
-            "items": {
-              "bsonType": "string"
-            }
-          },
-          "staffId": {
-            "bsonType": "int"
           }
-        }
-      }
-    },
-    "validationLevel": "strict",
-    "validationAction": "error"
-  });
-//   
-  db.createCollection("favorite", {
-    validator: {
-      $jsonSchema: {
-        "bsonType": "object",
-        "title": "favorite",
-        "required": ["favId", "dormId", "userId"],
-        "properties": {
-          "favId": {
-            "bsonType": "int"
-          },
-          "dormId": {
-            "bsonType": "int"
-          },
-          "userId": {
-            "bsonType": "int"
+        },
+        "building_facility": {
+          "bsonType": "array",
+          "items": {
+            "bsonType": "string"
           }
+        },
+        "room_facility": {
+          "bsonType": "array",
+          "items": {
+            "bsonType": "string"
+          }
+        },
+		"count_facilities": {
+          "bsonType": "int"
+        },
+		"score": {
+          "bsonType": "int"
+        },
+        "staffId": {
+          "bsonType": "int"
         }
       }
     }
-  });
+  },
+  "validationLevel": "strict",
+  "validationAction": "error"
+});
+// favorite
+db.createCollection("favorite", {
+  validator: {
+    $jsonSchema: {
+      "bsonType": "object",
+      "title": "favorite",
+      "required": ["favId", "dormId", "userId"],
+      "properties": {
+        "favId": {
+          "bsonType": "int"
+        },
+        "dormId": {
+          "bsonType": "int"
+        },
+        "userId": {
+          "bsonType": "int"
+        }
+      }
+    }
+  }
+});
+// form
+db.createCollection("form", {
+  validator: {
+    $jsonSchema: {
+      bsonType: "object",
+      title: "form",
+      required: ["formId", "form_date", "name", "email", "phone", "date_in", "date_out", "description", "staffId", "dormId", "userId"],
+      properties: {
+        formId: {
+          bsonType: "int"
+        },
+        form_date: {
+          bsonType: "date"
+        },
+        name: {
+          bsonType: "string"
+        },
+        email: {
+          bsonType: "string"
+        },
+        phone: {
+          bsonType: "string"
+        },
+        date_in: {
+          bsonType: "date"
+        },
+        date_out: {
+          bsonType: "date"
+        },
+		description: {
+          bsonType: "string"
+        },
+        staffId: {
+          bsonType: "int"
+        },
+        dormId: {
+          bsonType: "int"
+        },
+		userId: {
+          bsonType: "int"
+        }
+      }
+    }
+  }
+});
